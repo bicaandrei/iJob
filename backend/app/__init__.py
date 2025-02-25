601e932eb022adc1b2ac43bee462dec2daa0f430
bicaandrei <bica.nuna@gmail.com>
2025-02-25 18:12:27 +0200

    split init from routes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -6,6 +6,4 @@ env_path = '.env.dev' if os.getenv('FLASK_ENV') == 'development' else '.env.prod
 load_dotenv(dotenv_path=env_path)
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "<p>Hello, iJob!</p>"
+from . import routes
